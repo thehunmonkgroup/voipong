2b60f4afa7012d531c2cda939cd1b63e17077ca0
Chad Phillips <chad@apartmentlines.com>
2012-03-30 00:12:35 -0600

    attempt to fix loss of DTMF on PSTN calls.

--- app.js
+++ app.js
@@ -61,6 +61,10 @@ models.Call = Backbone.Model.extend({
   answer: function() {
     // Answer the call and emit an event.
     this._call.execute('answer');
+    // This is a hack to force some media over the channel, which smooths
+    // out provider problems with them not recognizing we're ready to receive
+    // data.
+    this._call.execute('playback', 'silence_stream://1000');
     this.trigger('answered');
   },
   keyPressed: function(args) {
